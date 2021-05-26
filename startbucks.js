caa14563750fbc8669d85e283a9be1d31f9e6e07
EunSungMoon <eunsung9647@gmail.com>
2021-05-26 22:31:15 +0900

    우유선택지, hide함수, 메뉴 알림창, 주문완료 텍스트

--- startbucks.js
+++ startbucks.js
@@ -10,123 +10,99 @@
   4. nxtStage -> 다음 btn evt 할당, dom 숨김
   5. hide -> dom 숨김 */
 
-const createEl = dom => document.createElement(dom);
 const el = selector => document.querySelector(selector);
 const elAll = selector => document.querySelectorAll(selector);
-
-let cname
-let temps
-let milkTypes
-let shots
-
-/*
-const selectMenu = document.querySelector('#radioBtn');
-selectMenu.addEventListener('change', e => { //메뉴 선택시 각 메뉴에 따른 디테일 메뉴화면으로 전환
-  MainMenu(e.target);
-})*/
-
-document.querySelectorAll('input[name="menu"]').forEach(dom => dom.addEventListener('change', e => {
-  coffeeMenu(e.target);
-}));
-
-
-
-const coffeeMenu = (target) => {
-  //화면을 지운다
-  const el = document.querySelector('#radioBtn');
-
-  // el.innerHTML='';
-  // //커피 오더 화면을 받는다
-  // el.innerHTML = newOrder[target.value](target.value);
-  el.style.display = 'none'
-  newOrder[target.value](target.value)
-
-}
-
-const newOrder = {
-  americano(value) {
-    // return `
-    //   <div>
-    //     <label><input type="checkbox" name="icehot" value="ice"/> 아이스 </label>
-    //     <label><input type="checkbox" name="icehot" value="hot"/> 핫 </label>
-    //   </div>
-    // `;
-    tempSelector(value)
-    nextStage(value)
-  },
-  latte(value) {
-    //라뗴를 그림
-    console.log('latte')
-  }
-};
 
 const hide = (event) => { //버튼 숨김 
   event.style.display = 'none'
 };
 
-const creatDiv = () => $(function () {
-  $("div.content").html('div')
-  let divDom = $('<div></div>')
-  $('#content').append(divDom);
-});
+elAll('input[name="menu"]').forEach(dom => dom.addEventListener('change', e => { //커피메뉴 선택 시 이벤트 
+  coffeeMenu(e.target);
+}));
 
-const tempSelector = () => { //hot, ice선택지
-  creatDiv()
+const coffeeMenu = (target) => { //화면 삭제, 아메리카노와 라떼에 해당하는 디테일 메뉴 선택화면 출력
+  const coffeeMenuBtn = el('#radioBtn');//아메리카노, 라떼 선택지를 화면에서 지운다
+  hide(coffeeMenuBtn);
 
-  // let temp = ['hot', 'ice']
+  el.innerHTML = '';
+  //커피 오더 화면을 받는다
+  el.innerHTML = newOrder[target.value](target.value);
+}
 
-  // const p = createEl('p');
-  // p.innerHTML = "hot, ice 선택"
-  // dom.appendChild(p);
-
-  // temp.forEach((tempValue, i) => {
-  //   let tempBtn = createEl('input')
-  //   let labelValue = createEl('label')
-
-  //   labelValue.innerHTML = tempValue
-  //   tempBtn.type = "radio"
-  //   tempBtn.id = temp[i]
-
-  //   dom.appendChild(labelValue);
-  //   dom.appendChild(tempBtn);
-  //   el('body').appendChild(dom);
-
-  // })
-
-  // elAll('#tempRadio').forEach(i => i.addEventListener('change', () => {
-  //   // e.target.dataset 활용 방법?
-  //   if (document.getElementById('hot').checked) {
-  //     temps = "hot"
-  //   }
-  //   else if (document.getElementById('ice').checked) {
-  //     temps = "ice"
-  //   }
-  // }))
+const newOrder = { //아메리카노, 라떼 디테일 화면출력 
+  americano(value) {
+    HotIce(value)
+    nextStage(value)
+  },
+  latte(value) {
+    HotIce(value)
+    milkTypes(value)
+    nextStage(value)
+  }
 };
 
+const milkTypes = () => {
+  let $milktypes = $(`<div id ="milktypes" ><label><input type="radio" name="milktype" value="original"/> 일반우유 </label>
+  <label><input type="radio" name="milktype" value="bean"/> 두유 </label>
+  <label><input type="radio" name="milktype" value="oats"/> 귀리우유 </label></div>`)
+  $('#content').append($milktypes)
 
-//1.  div 돔을 document.createElement() 를 통해서 만듬
+  elAll('input[name="milktype"]').forEach(dom => dom.addEventListener('change', e => {
+    milkTypesChoice(e.target);
+  }));
 
-//2. 1번의 div 돔에 appendChild 를 통해서 넣음
+  const milkTypesChoice = (target) => {
+    newmilkTypes[target.value](target.value)
+  }
+  const newmilkTypes = {
+    original(value) {
+      console.log("일반");
+    },
+    bean(value) {
+      console.log("두유");
+    },
+    oats(value) {
+      console.log("귀리");
+    }
+  }
 
+}
+
+const HotIce = () => { //hot, ice선택지
+  let $HotIce = $(`<div id ="hotice" ><label><input type="radio" name="icehot" value="ice"/> 아이스 </label>
+  <label><input type="radio" name="icehot" value="hot"/> 핫 </label></div>`)
+  $('#content').append($HotIce)
+
+
+  elAll('input[name="icehot"]').forEach(dom => dom.addEventListener('change', e => { //hot, ice 선택시 이벤트 발생
+    HotIceChoice(e.target);
+  }));
+
+  const HotIceChoice = (target) => {
+    newHotIce[target.value](target.value)
+  }
+
+  const newHotIce = {
+    ice(value) {
+      console.log("ice");
+    },
+    hot(value) {
+      console.log("hot");
+    }
+  }
+};
 
 const nextStage = () => { //다음 버튼
-  // const dom = createEl('div');
-  // dom.id = "next"
-  // let nextBtn = createEl('button');
-  // nextBtn.id = "nextBtn"
-  // nextBtn.innerHTML = "다음"
-
-
-
-  // dom.appendChild(nextBtn);
-  // el('body').appendChild(dom);
-
-  let $nextBtn=$('<button name ="next" value=next>다음</button>')
+  let $nextBtn = $('<button id="next" name ="next" value=next> 다음 </button>')
   $('#content').append($nextBtn)
 
-  // document.querySelectorAll('input[name="menu"]').forEach(dom => dom.addEventListener('change', e => {
-  //   coffeeMenu(e.target);//활용하기
+  el('#next').addEventListener('click', e => { //화면에 버튼 지우고, 알림창 등장 =>메뉴확인 후 끝!!
+    hide(next);
+    hide(hotice);
+    alert('선택하신 메뉴가 맞습니까? ') //선택한 메뉴 등장!!!
+    orderEnd();
+  })
 
   // el('#next').addEventListener('click', () => {
   //   hide(next);
@@ -138,19 +114,12 @@ const nextStage = () => { //다음 버튼
 };
 
 const orderEnd = () => {//완료 텍스트 출력
-  const dom = createEl('div');
-  dom.id = "end"
-  const p = createEl('p');
-  p.innerHTML = "주문이 완료 되었습니다."
 
-  dom.appendChild(p);
-  el('body').appendChild(dom);
+  $(function () {
+    let divDom = $('<div id= "end"><p>주문이 완료되었습니다.</p></div>')
+    $('#content').append(divDom);
+  });
 };
-
-
-
-
-
 
 
 
